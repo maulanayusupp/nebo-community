13a28c85636dafa40d75f70874e2ae70518ff3e3
Maulana <maulanayusupp@gmail.com>
2024-10-10 19:20:18 +0700

    Init

---
+++ pages/index.vue
@@ -0,0 +1,203 @@
+<template>
+<div>
+	<main>
+
+		<div class="relative isolate">
+			<svg class="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]" aria-hidden="true">
+				<defs>
+					<pattern id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+						<path d="M.5 200V.5H200" fill="none" />
+					</pattern>
+				</defs>
+				<svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+					<path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+				</svg>
+				<rect width="100%" height="100%" stroke-width="0" fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
+			</svg>
+			<div class="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48" aria-hidden="true">
+				<div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)" />
+			</div>
+			<div class="overflow-hidden">
+				<div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+					<div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+						<div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+							<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">NEBO
+							</h1>
+							<p class="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+								Super Sunday menghadirkan rangkaian perawatan kulit premium yang dirancang untuk memberikan kelembutan, kecerahan, dan hidrasi maksimal. Dengan bahan alami dan formula berkualitas tinggi, kami berkomitmen untuk menjaga kulit Anda tetap sehat dan terlindungi dari polusi serta stres sehari-hari.
+							</p>
+							<div class="mt-10 flex items-center gap-x-6">
+								<a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Beli sekarang</a>
+								<a href="#" class="text-sm font-semibold leading-6 text-gray-900">
+									Pelajari Lebih Lanjut <span aria-hidden="true">→</span>
+								</a>
+							</div>
+						</div>
+						<div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+							<div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+								<div class="relative">
+									<img src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+									<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+								</div>
+							</div>
+							<div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+								<div class="relative">
+									<img src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+									<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+								</div>
+								<div class="relative">
+									<img src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+									<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+								</div>
+							</div>
+							<div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+								<div class="relative">
+									<img src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+									<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+								</div>
+								<div class="relative">
+									<img src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+									<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Vision -->
+		<div class="overflow-hidden bg-white py-24 sm:py-32">
+			<div class="mx-auto max-w-7xl px-6 lg:px-8">
+				<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+					<div class="lg:pr-8 lg:pt-4">
+						<div class="lg:max-w-lg">
+							<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+								Mengapa Super Sunday?
+							</p>
+							<p class="mt-6 text-lg leading-8 text-gray-600">
+								Dengan tekstur ringan dan cepat meresap, Super Sunday nyaman digunakan tanpa rasa lengket atau berminyak.
+							</p>
+							<dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+								<div v-for="mission in missions" :key="mission.name" class="relative pl-9">
+									<dt class="inline font-semibold text-gray-900">
+										<component :is="mission.icon" class="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+										{{ mission.name }}
+									</dt>
+									{{ ' ' }}
+									<dd class="inline">{{ mission.description }}</dd>
+								</div>
+							</dl>
+						</div>
+					</div>
+					<img src="https://images.everydayhealth.com/images/skin-beauty/what-are-natural-skin-care-products-alt-1440x810.jpg" alt="Product screenshot" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442" />
+				</div>
+			</div>
+		</div>
+
+		<!-- Our Products -->
+		<div class="bg-white py-24 sm:py-32">
+			<div class="mx-auto max-w-7xl px-6 lg:px-8">
+				<div class="mx-auto max-w-2xl lg:text-center">
+					<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+						Produk Unggulan Kami
+					</p>
+					<p class="mt-6 text-lg leading-8 text-gray-600">Produk kami dikembangkan untuk menjaga kesehatan dan kecantikan kulit Anda secara optimal. Temukan produk yang paling sesuai dengan kebutuhan kulit Anda dan rasakan perbedaannya. Kecantikan sejati dimulai dari perawatan yang tepat.</p>
+				</div>
+				<div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+					<dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+						<div v-for="featureProduct in featureProducts" :key="featureProduct.name" class="flex flex-col">
+							<dt class="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+								<component :is="featureProduct.icon" class="h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+								{{ featureProduct.name }}
+							</dt>
+							<dd class="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+								<p class="flex-auto">{{ featureProduct.description }}</p>
+							</dd>
+						</div>
+					</dl>
+				</div>
+			</div>
+		</div>
+
+		<!-- CTA -->
+		<div class="bg-white">
+			<div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+				<div class="relative isolate overflow-hidden bg-gray-900 px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+					<h2 class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+						Dapatkan Kulit Sehat dengan Super Sunday!
+					</h2>
+					<p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
+						Mulai perjalanan perawatan kulit Anda dengan Super Sunday dan rasakan perbedaan nyata pada kulit Anda. Produk kami tersedia secara eksklusif melalui toko online kami dan mitra resmi.
+					</p>
+					<div class="mt-10 flex items-center justify-center gap-x-6">
+						<a href="#" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+							Beli Sekarang
+						</a>
+						<a href="#" class="text-sm font-semibold leading-6 text-white">
+							Pelajari Lebih Lanjut <span aria-hidden="true">→</span>
+						</a>
+					</div>
+					<svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
+						<circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7" />
+						<defs>
+							<radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+								<stop stop-color="#7775D6" />
+								<stop offset="1" stop-color="#E935C1" />
+							</radialGradient>
+						</defs>
+					</svg>
+				</div>
+			</div>
+		</div>
+
+	</main>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+import { StarIcon } from '@heroicons/vue/20/solid'
+
+const missions = [
+  {
+    name: 'Hidrasi Mendalam',
+    description: 'Formula Super Sunday memberikan kelembapan yang tahan lama, menjaga kulit tetap lembut dan sehat sepanjang hari.',
+    icon: StarIcon,
+  },
+  {
+    name: 'Perlindungan Alami',
+    description: 'Diperkaya dengan bahan alami yang menutrisi kulit dan melindunginya dari efek buruk lingkungan seperti polusi dan sinar UV.',
+    icon: StarIcon,
+  },
+  {
+    name: 'Tekstur Ringan dan Nyaman',
+    description: 'Super Sunday menyerap cepat tanpa rasa lengket, memberikan sensasi segar dan ringan di kulit.',
+    icon: StarIcon,
+  },
+  {
+    name: 'Cocok untuk Semua Jenis Kulit',
+    description: 'Dengan formula lembut dan non-irritasi, Super Sunday dapat digunakan untuk kulit sensitif, kering, berminyak, atau kombinasi.',
+    icon: StarIcon,
+  },
+]
+
+const featureProducts = [
+  {
+    name: 'Daily Sun Protection',
+    description: 'Pelindung matahari SPF tinggi dengan formula ringan yang melindungi kulit dari sinar UVA dan UVB, tanpa meninggalkan residu berminyak.',
+    icon: StarIcon,
+  },
+	{
+    name: 'Brightening Moisturizer',
+    description: 'Serum, masker, dan treatment khusus untuk masalah kulit seperti jerawat, penuaan dini, hiperpigmentasi, dan kulit sensitif.',
+    icon: StarIcon,
+  },
+	{
+    name: 'Hydrating Serum',
+    description: 'Mengunci kelembapan dan menjaga kulit tetap terhidrasi sepanjang hari dengan kombinasi bahan alami seperti hyaluronic acid dan ekstrak lidah buaya.',
+    icon: StarIcon,
+  },
+]
+</script>
