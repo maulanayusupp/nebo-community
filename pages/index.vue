db1c39f2141fe08dc14c0304d141e4bca17da100
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2024-10-21 07:56:21 +0700

    Add CTA

--- pages/index.vue
+++ pages/index.vue
@@ -106,6 +106,52 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- CTA -->
+		<div class="overflow-hidden bg-white py-32">
+			<div class="mx-auto max-w-7xl px-6 lg:flex lg:px-8">
+				<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
+					<div class="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
+						<h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							Gabung sekarang juga!
+							<br>
+							Start your journey with us.
+						</h2>
+						<p class="mt-6 text-xl leading-8 text-gray-600 mb-8">Bergabunglah sekarang dengan komunitas NMAX! Temukan teman, pengalaman, dan petualangan seru bersama kami.</p>
+						<div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+							<a
+								href="https://wa.me/6281223230300?text=Halo,%20saya%20ingin%20bergabung%20dengan%20NEBO:%20Yamaha%20NMAX%20Neo%20Turbo%20Community.%20Mohon%20informasinya%20lebih%20lanjut.%20Terima%20kasih!"
+								target="_blank"
+								class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+								Gabung sekarang
+							</a>
+							<a
+								href="https://wa.me/6285722437493?text=Halo,%20saya%20ingin%20bergabung%20dengan%20NEBO:%20Yamaha%20NMAX%20Neo%20Turbo%20Community.%20Mohon%20informasinya%20lebih%20lanjut.%20Terima%20kasih!"
+								target="_blank"
+								class="text-sm font-semibold leading-6 text-gray-900">
+								Hubungi Admin <span aria-hidden="true">→</span>
+							</a>
+						</div>
+					</div>
+					<div class="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
+						<div class="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
+							<img src="/assets/images/hero/hero_1.jpeg" alt="" class="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
+						</div>
+						<div class="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
+							<div class="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
+								<img src="/assets/images/hero/hero_2.jpeg" alt="" class="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
+							</div>
+							<div class="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
+								<img src="/assets/images/hero/hero_3.jpeg" alt="" class="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover" />
+							</div>
+							<div class="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
+								<img src="/assets/images/hero/hero_4.jpeg" alt="" class="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 </div>
 </template>
