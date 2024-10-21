162cfc47894f7cea549e21c9bff8f66e1cfeed8a
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2024-10-21 08:04:39 +0700

    QUICK FIX AND UPDATE COMMIT: Add/Update fixes components

--- layouts/partials/TheFooter.vue
+++ layouts/partials/TheFooter.vue
@@ -4,9 +4,6 @@
     <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
         <div>
-					<p class="text-sm text-gray-600 mb-3 leading-6">
-						<label for="">NEBO: Yamaha NMAX Neo Turbo Community</label>
-					</p>
 					<img class="h-28 rounded-lg" src="/assets/images/logos/nebo_logo.jpeg" alt="Company name" />
 				</div>
         <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
@@ -43,6 +40,15 @@
           </div>
         </form>
       </div>
+			<div class="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
+        <div class="flex space-x-6 md:order-2">
+          <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
+            <span class="sr-only">{{ item.name }}</span>
+            <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+          </a>
+        </div>
+        <p class="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">&copy; 2024 NEBO: NMAX Neo Turbo Community. All rights reserved.</p>
+      </div>
     </div>
   </footer>
 </template>
@@ -68,7 +74,7 @@ const navigation = {
   social: [
     {
       name: 'Facebook',
-      href: '#',
+      href: 'https://www.instagram.com/nebo_nmaxneoturbo',
       icon: defineComponent({
         render: () =>
           h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
@@ -82,7 +88,7 @@ const navigation = {
     },
     {
       name: 'Instagram',
-      href: '#',
+      href: 'https://www.instagram.com/nebo_nmaxneoturbo',
       icon: defineComponent({
         render: () =>
           h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
@@ -95,20 +101,8 @@ const navigation = {
       }),
     },
     {
-      name: 'X',
-      href: '#',
-      icon: defineComponent({
-        render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-            h('path', {
-              d: 'M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z',
-            }),
-          ]),
-      }),
-    },
-    {
       name: 'YouTube',
-      href: '#',
+      href: 'https://www.instagram.com/nebo_nmaxneoturbo',
       icon: defineComponent({
         render: () =>
           h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
