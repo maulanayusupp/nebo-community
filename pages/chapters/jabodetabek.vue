34151817f6b185c983c1533fcfb50618e39b142e
Maulana <maulanayusupp@gmail.com>
2025-03-10 08:27:05 +0700

    feat: add jabodetabek

---
+++ pages/chapters/jabodetabek.vue
@@ -0,0 +1,133 @@
+<template>
+	<div>
+		<main>
+
+			<!-- Intro -->
+			<div class="relative isolate">
+				<svg class="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]" aria-hidden="true">
+					<defs>
+						<pattern id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
+							<path d="M.5 200V.5H200" fill="none" />
+						</pattern>
+					</defs>
+					<svg x="50%" y="-1" class="overflow-visible fill-gray-50">
+						<path d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z" stroke-width="0" />
+					</svg>
+					<rect width="100%" height="100%" stroke-width="0" fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)" />
+				</svg>
+				<div class="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48" aria-hidden="true">
+					<div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)" />
+				</div>
+				<div class="overflow-hidden">
+					<div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+						<div class="mb-8">
+							<img src="/assets/images/chapters/jabodetabek.png" alt="" class="max-w-[400px] mx-auto rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+						</div>
+						<div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+							<div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+								<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Selamat Datang di NEBO Jabodetabek!
+								</h1>
+								<p class="mt-6 text-md leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+									Senang sekali bisa menyambut kamu sebagai bagian dari keluarga besar NEBO Jabodetabek. Di sini, kita tidak hanya berbagi pengalaman, ilmu, dan cerita tentang motor, tapi juga menjalin persaudaraan yang erat di setiap perjalanan.
+									<br>
+									Mari bersama-sama menikmati setiap kilometer perjalanan, memperkuat solidaritas, dan menciptakan kenangan yang tak terlupakan. Jika ada yang perlu ditanyakan atau dibagikan, jangan ragu untuk menghubungi teman-teman NEBO lainnya.
+								</p>
+
+								<div class="mt-4 mb-6 font-bold">
+									Yuk, Gabung Gratis ke NEBO – Komunitas Seru Bagi Pengguna Yamaha NMAX!
+									<div class="mt-2 font-semibold text-xs text-gray-500">*Pada saat daftar sertakan foto atau unit Anda untuk verifikasi.</div>
+								</div>
+								<div class="mt-10 grid grid-cols-2 space-y-3 md:space-y-0 md:flex items-center gap-x-3 lg:mt-0 lg:flex-shrink-0">
+									<a
+										:href="`https://wa.me/6285156685691?text=${registerMessage}`"
+										target="_blank"
+										class="text-sm font-semibold leading-6 text-gray-900">
+										Hubungi Rekrutmen <span aria-hidden="true">→</span>
+									</a>
+									<a
+										:href="`https://wa.me/6285156685691?text=${registerMessage}`"
+										target="_blank"
+										class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+										Rekrutmen 1
+									</a>
+									<a
+										:href="`https://wa.me/6287886939070?text=${registerMessage}`"
+										target="_blank"
+										class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+										Rekrutmen 2
+									</a>
+								</div>
+							</div>
+							<div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
+								<div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
+									<div class="relative">
+										<img src="/assets/images/hero/hero_1.jpeg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+										<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+									</div>
+								</div>
+								<div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
+									<div class="relative">
+										<img src="/assets/images/hero/hero_2.jpeg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+										<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+									</div>
+									<div class="relative">
+										<img src="/assets/images/hero/hero_3.jpeg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+										<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+									</div>
+								</div>
+								<div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
+									<div class="relative">
+										<img src="/assets/images/hero/hero_4.jpeg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+										<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+									</div>
+									<div class="relative">
+										<img src="/assets/images/hero/hero_5.jpeg" alt="" class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg" />
+										<div class="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</main>
+	</div>
+	</template>
+
+	<script setup>
+	import { ref } from 'vue'
+
+	import { StarIcon } from '@heroicons/vue/20/solid'
+	import HeroSection from '~/components/home/HeroSection.vue';
+
+	const registerMessage = "Selamat%20Datang%20di%20FORUM%20NEBO%20NMAX%20Neo%20Turbo%20Indonesia!%0A%0AHalo!%20Silakan%20perkenalkan%20diri%20dengan%20format%20berikut:%0A%0ANama%20Asli:%20NAMA%20ANDA%20(NAMA%20PANGGILAN)%0AAlamat:%20ALAMAT%20ANDA%0AKegiatan%20Sehari-hari:%20KEGIATAN%20SEHARI%20HARI%0AFoto/Video%20Motor:%20KIRIM%20FOTO%20MOTOR/SELFIE%0A%0AMari%20kita%20jalin%20silaturahmi,%20menjaga%20nama%20baik,%20dan%20membawa%20berkah%20dalam%20setiap%20pertemuan!";
+
+	const missions = [
+		{
+			name: 'Koneksi dengan Sesama Penggemar',
+			description: 'Dapatkan kesempatan untuk bertemu dan berinteraksi dengan para pemilik dan penggemar Yamaha NMAX lainnya. Berbagi pengalaman dan tips modifikasi akan memperkaya pengetahuan Anda tentang motor kesayangan.',
+			icon: StarIcon,
+		},
+		{
+			name: 'Dukungan dan Saran Modifikasi',
+			description: 'Anggota NEBO saling membantu dalam hal modifikasi motor. Anda akan mendapatkan saran berharga tentang peningkatan performa dan gaya yang dapat membuat NMAX Anda semakin keren dan bertenaga.',
+			icon: StarIcon,
+		},
+		{
+			name: 'Kegiatan Seru dan Touring',
+			description: 'Ikuti berbagai kegiatan menarik, mulai dari kopdar hingga touring bersama. Rasakan kebersamaan dan keseruan menjelajahi jalanan dengan teman-teman satu hobi.',
+			icon: StarIcon,
+		},
+		{
+			name: 'Akses ke Informasi Terbaru',
+			description: 'Dapatkan informasi terkini tentang produk, event, dan tren modifikasi Yamaha NMAX. Jangan lewatkan kesempatan untuk selalu update dan mendapatkan penawaran spesial dari mitra komunitas.',
+			icon: StarIcon,
+		},
+		{
+			name: 'Komunitas yang Ramah dan Inklusif',
+			description: 'NEBO adalah tempat di mana semua penggemar NMAX disambut dengan hangat, tanpa memandang pengalaman berkendara atau tingkat modifikasi. Bergabunglah dengan komunitas yang mendukung dan menginspirasi satu sama lain.',
+			icon: StarIcon,
+		},
+	]
+	</script>
